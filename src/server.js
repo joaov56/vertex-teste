78712c91c7c9dd8dc27e76982a4fbb4426d57aa8
unknown <vitorjao56@gmail.com>
2020-06-10 12:57:54 -0300

    adiciona requisições para api do youtube com axios

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express= require('express');
+const axios = require('axios')
 
 const server= express();
+
 
 server.use(express.static("public"));
 
 server.use(express.urlencoded({extended: true}))
+
+
+
+
+const ytkey= 'AIzaSyAZXCvJRJIgbJyn1f-gRp3dW4E23oruE7A'
 
 
 
@@ -16,7 +23,21 @@ nunjucks.configure("src/views", {
 
 
 server.get('/', (req, res)=>{
+
+    const request =axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&pageToken=CAoQAA&q=ok%20dog&type=video&key=AIzaSyAZXCvJRJIgbJyn1f-gRp3dW4E23oruE7A`)
+    .then(function(response){
+        console.log(response.data.nextPageToken);
+        for(var i = 1; i<=10; i++){           
+            console.log(response.data.items[i]);
+        }
+        
+        
+        
+    })
+
     return res.render("index.html")
+  
+       
     
 })
 
